e9946e8d2a0c1804666108ce1950b5494cc79cc5
Matias Cejas <matiascejas8@gmail.com>
2020-09-18 13:24:40 -0300

    Basic example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Formik } from "formik";
+import * as Yup from "yup";
+import {  DisplayFormikState } from "./helper";
+import "./helper.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+    <h1>
+      Formik demo
+    </h1>
+
+    <Formik
+      initialValues={{ email: "" }}
+      onSubmit={async values => {
+        await new Promise(resolve => setTimeout(resolve, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .email()
+          .required("Required")
+      })}
+    >
+      {props => {
+        const {
+          values,
+          touched,
+          errors,
+          dirty,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          handleReset
+        } = props;
+        return (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email" style={{ display: "block" }}>
+              Email
+            </label>
+            <input
+              id="email"
+              placeholder="Enter your email"
+              type="text"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={
+                errors.email && touched.email
+                  ? "text-input error"
+                  : "text-input"
+              }
+            />
+            {errors.email && touched.email && (
+              <div className="input-feedback">{errors.email}</div>
+            )}
+
+            <button
+              type="button"
+              className="outline"
+              onClick={handleReset}
+              disabled={!dirty || isSubmitting}
+            >
+              Reset
+            </button>
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+
+            <DisplayFormikState {...props} />
+          </form>
+        );
+      }}
+    </Formik>
+  </div>
   );
 }
 
